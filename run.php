596043b416bc549525816db281236ba7edc42ac2
Zeolite <tf-91@mail.ru>
2018-01-04 00:06:57 +0300

    Use composer lib bracketeer

--- run.php
+++ run.php
@@ -6,13 +6,17 @@
  * Date: 03.01.18
  * Time: 12:14
  */
+require_once 'vendor/autoload.php';
+use Kargin\Bracketeer;
+
+$bracketeer = new Bracketeer();
 
 function printBool($b) {return ($b) ? 'OK' : 'NOT OK';}
 
 $test_string=file_get_contents('/home/zeolite/OTUS/HW1/composer_bracketeer/src/string');
 echo $test_string;
 try {
-    echo printBool(isBalanced($test_string));
+    echo printBool($bracketeer->isBalanced($test_string));
 } catch (Exception $e) {
     echo get_class($e) . ": " . $e->getMessage();
 }
