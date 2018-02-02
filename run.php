777962d7c9e59c524c57a9b7e43d275b8863baf0
Zeolite <tf-91@mail.ru>
2018-02-02 03:06:42 +0300

    Rename git.ignore to .gitignore
    Made little code refactoring, updated composer.lock.

--- run.php
+++ run.php
@@ -1,34 +1,48 @@
 #!/usr/bin/env php
 <?php
-/**
- * Created by PhpStorm.
- * User: zeolite
- * Date: 03.01.18
- * Time: 12:14
- */
+
 require_once 'vendor/autoload.php';
 use Kargin\Bracketeer;
 
-$bracketeer = new Bracketeer();
+/**
+ * Prints result based on boolean value
+ *
+ * @param $b
+ * @return string
+ */
+function printBool($b) {
+    return $b ? "OK. Brackets in given string ARE balanced.\n": "ERROR. Brackets in given string are NOT balanced.\n";
 
-function printBool($b) {return $b ? "OK. Brackets in given string ARE balanced.\n": "ERROR. Brackets in given string are NOT balanced.\n";}
-function warning_handler($errno, $errstr) {
+}
+
+/**
+ * Handles warnings
+ *
+ * @param $errno
+ * @param $errstr
+ */
+function warningHandler($errno, $errstr) {
  echo "ERROR\n$errstr\nERROR CODE = $errno\n";
 }
+
+$bracketeer = new Bracketeer();
+
 echo "Press \"Enter\" for using one of three default tests.\n";
 echo "Enter path to file that contains string with bracket problem [tests/test_1]: \n";
+
 $line = readline();
+
 if (empty($line)) {
     $line = 'tests/test_1';
 }
 
-set_error_handler("warning_handler", E_WARNING);
-$test_string = file_get_contents($line);
+set_error_handler("warningHandler", E_WARNING);
+$testString = file_get_contents($line);
 restore_error_handler();
 
-if ($test_string !== FALSE) {
+if ($testString !== false) {
     try {
-        echo printBool($bracketeer->isBalanced($test_string));
+        echo printBool($bracketeer->isBalanced($testString));
     } catch (Exception $e) {
         echo get_class($e) . ":\n";
         echo $e->getMessage() . "\n";
